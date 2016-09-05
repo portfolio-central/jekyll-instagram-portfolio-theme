f1a0ddbd260ddb16bf4e3ddb3f5e3f3740162999
Sergey Sokurenko <s.sokurenko@gmail.com>
2016-09-05 23:34:18 +0200

    Added Javascript animations, refactored web assets and the build script.

--- web-dist/js/app.js
+++ web-dist/js/app.js
@@ -1,13 +1,41 @@
 $(document).ready(function () {
 
+    // Selecting some DOM elements for further handling
+
+    var dom = {
+        window: $(window),
+        body: $('body'),
+        navItem: $('.nav-item'),
+        navBarLogo: $('.navbar-logo')
+    };
+
     // Handling the navigation links
 
-    $('.nav-item').click(function () {
+    dom.navItem.click(function () {
         var scrollElement = $(this).find('.nav-link').data('link'),
             scrollPosition = $(scrollElement).offset().top - 100;
 
-        $('html, body').animate({
+        dom.body.animate({
             scrollTop: scrollPosition
         }, 500);
+    });
+
+    // Displaying the logo at the navigation bar
+
+    dom.window.scroll(function () {
+
+        if (dom.window.scrollTop() >= 300) {
+            dom.navBarLogo.addClass('show');
+        } else {
+            dom.navBarLogo.removeClass('show');
+        }
+    });
+
+    // Scroll to top on the logo click
+
+    dom.navBarLogo.click(function () {
+        dom.body.animate({
+            scrollTop: 0
+        }, 300);
     });
 });
